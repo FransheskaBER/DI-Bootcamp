0eb7a19bcffab02b55ddc0e09662996dffd6013e
FransheskaBER <fransheskaechevarria@gmail.com>
2025-07-26 10:45:46 +0300

    new organization

--- Week1/Day1/basic_values.py
+++ Week1/Day1/basic_values.py
@@ -2,27 +2,27 @@
 
 # To store data/values in your code, you need to use the symbol = with a variable(key) and its value, so you have "variable = value" then you can refer just to variable itself since the value of that variable is already store and everytime youuse the variable, the code knows what value that variable have.
 
-# greetings = 'hello world'
-# print(greetings.capitalize()) #so the phrase will start with upper case, only the first letter of the sentence
+greetings = 'hello world'
+print(greetings.capitalize()) #so the phrase will start with upper case, only the first letter of the sentence
 
 # # By adding a string method like .capitalize() are functions that you can apply into a string
 # # More functions that you can apply to Strings: .upper() .lower() 
 
-# print(greetings.upper())
-# print(greetings.lower())
-# print(greetings[0])
-# print(greetings[1])
-# print(greetings[2])
-# print(greetings[3])
-# print(greetings[4])
-# print(greetings[5:11])
+print(greetings.upper())
+print(greetings.lower())
+print(greetings[0])
+print(greetings[1])
+print(greetings[2])
+print(greetings[3])
+print(greetings[4])
+print(greetings[5:11])
 
-# description = "strings are..."
+description = "strings are..."
 
-# print(description.upper())
-# print(description[:7]) #from O to 7 ":7" to print fom 7 to the edn then "7:" 
-# print(description[0:7])
-# print(description.replace("are", "is")) #Use replace() to replace a string and add the first string you want to replace in "" comma and the the string you want to replace it with in ""
+print(description.upper())
+print(description[:7]) #from O to 7 ":7" to print fom 7 to the edn then "7:" 
+print(description[0:7])
+print(description.replace("are", "is")) #Use replace() to replace a string and add the first string you want to replace in "" comma and the the string you want to replace it with in ""
 # # what is inside the  purple () is called "arguments" - it means what and to what instead
 
 #NUMBERS - integers (whole numbers), floats (decimals), complex numbers (numbers with letters).
---
+++ Week1/Day1/exercises.py
@@ -0,0 +1,96 @@
+
+# description = "strings are..."
+# print(description)
+# print(description.upper())
+# print(description.replace("are", "is"))
+# print(description.replace("are", "ARE"))
+# print(description[0:7])
+
+# my_age = 33
+
+# print(my_age)
+# print(type(my_age))
+# print(my_age + 123879)
+# print(f"In 123879 years, I will be {int(my_age + 123879)}")
+
+# bank_balance = "33000"
+# # print(type(bank_balance))
+# print(type(int(bank_balance)))
+
+# phone_number = 532287514
+# print(type(phone_number))
+# print(type(str(phone_number)))   
+
+# first_name = "Fransheska"
+# last_name = "Echevarria"
+# print(first_name + " " + last_name)
+# print(first_name, last_name)
+
+# print("hello world\nHow are you?")
+# print("Write your name\tNOW")
+
+# x = 5
+# y = 10
+# z = 0
+# word1 = "hello"
+# word2 = "world"
+
+# if x < y and y > z:
+#     print("true")
+
+# if word1 == word2:
+#     print("they are the same word")
+# else:
+#     print("they're not the same word")
+
+# print(bool(z))
+# print(bool(word1))
+
+# name = "Alice"
+# age = "30"
+# city = "New York"
+# print(f"hello, {name}! You are {age} years old and live in {city}")
+# print("Hello, {}! You are {} years old and live in {}" .format(name, age, city))
+
+# name = (input("What's your name?"))
+# print("Nice to meet you", name)
+# age = (input("What's your age?"))
+# print(f"Wow, you will be turn 100 in {100 - int(age)} years")
+
+# number = input("what's your favorite number?")
+# print(f"your favorite number multiply by 3 is {int(number) * 3}")
+
+# age = int(input("how old are you? "))
+
+# if age >= 30:
+#     print("you are in your 30s")
+# elif age < 30 and age >= 20:
+#     print("you are in your 20s")
+# else:
+#     print("you are too young")
+
+# name = input("what's your name ")
+# if len(name) < 5:
+#     print("You have a short name")
+# elif len(name) > 5 and len(name) < 11:
+#     print("your name is long")
+# else:
+#     print("your name is TOO LONG!")
+
+# hobbies = "hiking, dancing"
+# if "code" in hobbies:
+#     print("You will be soon a dev")
+# else:
+#     print("you need to study code")
+
+# number = int(input("enter the secret number "))
+# if number % 3 == 0 and number % 5 ==0:
+#     print("FizzBuzz")
+# elif number % 3 == 0:
+#     print("Fizz")
+# elif number % 5 == 0:
+#     print("Buzz")
+
+
+
+
