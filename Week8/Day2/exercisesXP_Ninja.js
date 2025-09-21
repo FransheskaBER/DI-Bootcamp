0a4a01d157a932ed380552838b8b5db59e618fc4
Fransheska <fransheskaechevarria@gmail.com>
2025-09-21 15:31:34 +0300

    W8D2

--- Week8/Day2/exercisesXP_Ninja.js
+++ Week8/Day2/exercisesXP_Ninja.js
@@ -53,8 +53,127 @@ function calculateTip(){
 
 };
 
+// Second Part:
+// To avoid displaying the total if the function calculateTip() is not called, add the CSS property “display:none” to the tag which id is totalTip.
+// Call the function calculateTip() when the tag which id is calculate is clicked.
+// Hint : use the method onclick.
+
 const calBtn = document.querySelector("#calculate");
-calBtn.addEventListener("click", calculateTip);
+// calBtn.addEventListener("click", calculateTip);
+calBtn.onclick = calculateTip;
+
+
+
+
+// Exercise 2 : Validate the email
+// Instructions
+// We will create a form that ask the user for their email. Then, using a function we will check the validity of the user’s input.
+
+// Add an input that has a type="email" to your form.
+// Write your own javascript validation function to check if the entered value is a valid email address. The address should contain some valid characters,
+// and the @ sign, more characters then a . (period) and some more characters.
+// On “submit”, the validation function should run and validate the email address.
+// Try to do this exercise twice : with and without regex.
+
+let divForm = document.querySelector("#form");
+divForm.textContent = "NEW FORM:"
+
+let form = document.createElement("form");
+let p = document.createElement("p");
+p.textContent = "Enter your email "
+
+let input = document.createElement("input");
+input.type = "email";
+input.placeholder = "name@example.com";
+
+let submit = document.createElement("button");
+submit.type = "submit";
+submit.textContent = "Submit";
+
+p.appendChild(input);
+form.appendChild(p);
+form.appendChild(submit);
+divForm.appendChild(form);
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+
+// function withRegex(e){
+//     e.preventDefault();
+
+//     const value = input.value
+
+//     if (emailRegex.test(value)){
+//        let successText = document.createElement("p");
+//        successText.textContent = `Email Submitted: ${value}`;
+//        divForm.appendChild(successText);
+
+//     } else {
+//         alert("Enter a valid email")
+//     }
+// }
+
+function withoutRegex(e){
+    e.preventDefault();
+
+    if (input.checkValidity()){
+    let successText = document.createElement("p");
+    successText.textContent = `Email Submitted: ${value}`;
+    divForm.appendChild(successText);
+} else {
+    alert("Enter a valid email")
+}};
+
+
+submit.addEventListener("click", withoutRegex);
+
+
+
+// Exercise 3 : Get the user’s geolocation coordinates
+// Instructions
+// Hint/tip
+
+// Use HTML5 and Javascript for this exercise.
+// Use everything we learned in class, and use all the resources linked in the course.
+// This exercise is a bit tricky, search Google how to work with HTML5 and the navigator’s geolocation.
+// Create a button
+// For example, after the user clicks on the button, the output of your code should be as seen below :
+
+// Latitude: 32.179346699999996
+// Longitude: 34.871555
+
+let locationdiv = document.querySelector("#location")
+
+let getlocation = document.createElement("button");
+getlocation.textContent = "Get Current Location"
+
+locationdiv.appendChild(getlocation);
+
+getlocation.addEventListener("click", () => {
+    if (!navigator.geolocation){
+        console.log("Geolocation is not supported by your browser");
+        return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            console.log(`Latitude: ${lat}\nLongitude: ${lon}`);
+        },
+
+        (error) => {
+            if (error.code === 1){
+                console.error("User denied the request for Geolocation.");
+            } else if (error.code === 2){
+                console.error("Location unavailable.");
+            } else if(error.code === 3){
+                console.error("Request timed out.");
+            } else {
+                console.error("Unknown error:", error.message);
+            }
+        }
+    )
+})
 
 
 
@@ -63,10 +182,4 @@ calBtn.addEventListener("click", calculateTip);
 
 
 
-
-
-    // Second Part:
-    // To avoid displaying the total if the function calculateTip() is not called, add the CSS property “display:none” to the tag which id is totalTip.
-    // Call the function calculateTip() when the tag which id is calculate is clicked.
-    // Hint : use the method onclick.
 
