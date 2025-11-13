cbaad13ddaf7af3d6c3002c1694b12258184808b
Fransheska <fransheskaechevarria@gmail.com>
2025-11-13 19:32:46 +0200

    exerciseXP

--- Week_16/Day6/ExerciseXP/my-react-app/src/BuggyCounter.jsx
+++ Week_16/Day6/ExerciseXP/my-react-app/src/BuggyCounter.jsx
@@ -1,14 +1,5 @@
 import { useState } from "react";
 
-// export default function ErrorFallback({ error }){
-//     return (
-//         <div role="alert">
-//             <h3>Something went wrong!</h3>
-//             <pre>{error.message}</pre>
-//         </div>
-//     );
-// }
-
 export default function BuggyCounter(){
     const [count, setCount] = useState(0);
 
