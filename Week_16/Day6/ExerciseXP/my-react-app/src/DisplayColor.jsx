2756dfe395928c80644e80932bd6307640ced01b
Fransheska <fransheskaechevarria@gmail.com>
2025-11-13 19:25:01 +0200

    exercise XP

--- Week_16/Day6/ExerciseXP/my-react-app/src/DisplayColor.jsx
+++ Week_16/Day6/ExerciseXP/my-react-app/src/DisplayColor.jsx
@@ -27,3 +27,31 @@ function Child({ show }){
 }
 
 export default React.memo(Child);
+
+// from BuggyCounter.jsx:
+// import { useState } from "react";
+
+// export default function ErrorFallback({ error }){
+//     return (
+//         <div role="alert">
+//             <h3>Something went wrong!</h3>
+//             <pre>{error.message}</pre>
+//         </div>
+//     );
+// }
+
+// export default function BuggyCounter(){
+//     const [count, setCount] = useState(0);
+
+//     function HandleClick(){
+//         setCount(prev => prev + 1);
+//     }
+
+//     if (count === 5){
+//         throw new Error("CRASHED!!");
+//     }
+
+//     return (
+//         <button onClick={HandleClick} style={{ fontSize: "2rem" }}>{count}</button>
+//     );
+// }
